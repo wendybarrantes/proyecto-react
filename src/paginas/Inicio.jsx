f5b3a0009e393a7ecf79a1d8be9fbc40ebf93a6d
wendybarrantes <wenbarrantes06@gmail.com>
2024-09-20 10:30:01 -0600

    creacion pagina inicio

--- src/paginas/Inicio.jsx
+++ src/paginas/Inicio.jsx
@@ -1,24 +1,13 @@
 import Header from "../componentes/Header"
-import ListaCards from "../componentes/ListaCards"
-import { useEffect, useState } from "react";
-import { getData } from "../servicios/fetch";
 
 const Inicio = ()=>{
-    const [platillos,setPlatillos] = useState([])
-    useEffect(()=>{
-        const traerPlatillos = async()=>{
-            const datos = await getData("productos")
-            setPlatillos(datos)
-        }
-        traerPlatillos()
-    },[platillos])
+    
     return(
         <>
         <Header/>
             <h1>Le damos la bienvenida a Mossi Food Service ❤️</h1>
         
         <h2>Nuestro menu es</h2>
-        <ListaCards platillos={platillos}/>
         </>
     )
 }
